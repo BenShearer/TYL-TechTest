0b3044ea1c49962d9ac4097994a1532a80cd681b
Ben Shearer <ben.shearer@me.com>
2022-07-05 11:01:26 +0100

    push build

--- src/LSE.TradeHub/LSE.TradeHub.API/Program.cs
+++ src/LSE.TradeHub/LSE.TradeHub.API/Program.cs
@@ -14,7 +14,6 @@ namespace LSE.TradeHub.API {
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddControllers();
-
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddAutoMapper(typeof(AutomapperProfile));
